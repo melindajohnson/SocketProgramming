3b0a252e07a94d914684671699ab2118bbf646b5
meljo <melistajohn@gmail.com>
2020-06-01 18:03:22 -0700

    some changes made

--- Client.cpp
+++ Client.cpp
@@ -18,18 +18,171 @@
 * (3) sending the number of reads which the server performed.
 */
 
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+#include <netdb.h>
+#include <netinet/tcp.h>
+#include <sys/uio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
 
-// * Your client program will take the following six arguments:
-//1.	serverName:  the name of the server
-//2.	port: the IP port number used by server (use the last 5 digits of your student id)
-//3.	repetition: the repetition of sending a set of data buffers
-//4.	nbufs: the number of data buffers
-//5.	bufsize: the size of each data buffer (in bytes)
-//6.	type: the type of transfer scenario: 1, 2, or 3
-//For example,
-//// validate the arguments
-//	if ( argc != 7 )
-//	{
-//		printf( "usage: client #serverName #port #repetition #nbufs #bufsize #type \n" );
-//		return -1;
-//	}
+const int BUFFSIZE=1500;
+
+/**
+ * Multiple write test
+ * @param clientSd file descriptor of client socket
+ * @param databuf data buffer
+ * @param nbufs number of buffers
+ * @param bufsize buffer size
+ */
+void Test1(int clientSd,char **databuf,int nbufs, int bufsize)
+{
+    for (int j = 0; j < nbufs; j++)
+    {
+        write(clientSd, databuf[j], bufsize);
+    }
+}
+
+/**
+ * Writev test
+ * @param clientSd file descriptor of client socket
+ * @param databuf data buffer
+ * @param nbufs number of buffers
+ * @param bufsize buffer size
+ */
+void Test2(int clientSd,char **databuf,int nbufs, int bufsize)
+{
+    struct iovec vector[nbufs];
+    for (int j = 0; j < nbufs; j++)
+    {
+        vector[j].iov_base = databuf[j];
+        vector[j].iov_len = bufsize;
+    }
+    writev(clientSd, vector, nbufs);
+
+}
+
+/**
+ * Single write test
+ * @param clientSd file descriptor of client socket
+ * @param databuf data buffer
+ * @param nbufs number of buffers
+ * @param bufsize buffer size
+ */
+void Test3(int clientSd,char **databuf,int nbufs, int bufsize)
+{
+    write(clientSd, databuf, nbufs * bufsize);
+}
+
+
+int main(int argc, char *argv[])
+{
+
+     // Argument validation
+    if ( argc != 7 )
+	{
+		printf( "usage: client #serverName #port #repetition #nbufs #bufsize #type \n" );
+		return -1;
+	}
+
+    char *serverName = argv[1];   //the name of the server
+    char *serverPort = argv[2]; //the IP port number used by server (use the last 5 digits of your student id)
+    int repetition = atoi(argv[3]); //the repetition of sending a set of data buffers
+    int nbufs = atoi(argv[4]); //the number of data buffers
+    int bufsize = atoi(argv[5]); //the size of each data buffer (in bytes)
+    int type = atoi(argv[6]); //the type of transfer scenario: 1, 2, or 3
+    char databuf[nbufs*bufsize];
+    //char *databuf;
+    int clientSD = -1;
+
+    /*
+     * Use getaddrinfo() to get addrinfo structure corresponding to serverName / Port
+	 * This addrinfo structure has internet address which can be used to create a socket too
+     */
+
+    struct addrinfo hints;
+    struct addrinfo *result, *rp;
+    memset(&hints, 0, sizeof(struct addrinfo));
+    hints.ai_family = AF_UNSPEC;					/* Allow IPv4 or IPv6*/
+    hints.ai_socktype = SOCK_STREAM;					/* TCP */
+    hints.ai_flags = 0;							/* Optional Options*/
+    hints.ai_protocol = 0;						/* Allow any protocol*/
+    int rc = getaddrinfo(serverName, serverPort, &hints, &result);
+    if (rc != 0)
+    {
+        cerr << "ERROR: " << gai_strerror(rc) << endl;
+        exit(EXIT_FAILURE);
+    }
+
+    /*
+     * Iterate through addresses and connect
+     */
+    for (rp = result; rp != NULL; rp = rp->ai_next)
+    {
+        clientSD = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
+        if (clientSD == -1)
+        {
+            continue;
+        }
+        /*
+        * A socket has been successfully created
+        */
+        rc = connect(clientSD, rp->ai_addr, rp->ai_addrlen);
+        if (rc < 0)
+        {
+            cerr << "Connection Failed" << endl;
+            close(clientSD);
+            return -1;
+        }
+        else	//success
+        {
+            break;
+        }
+    }
+
+    if (rp == NULL)
+    {
+        cerr << "No valid address" << endl;
+        exit(EXIT_FAILURE);
+    }
+    else
+    {
+        cout << "Client Socket: " << clientSD << "  connected"<< endl;
+    }
+    freeaddrinfo(result);
+
+    //databuf = new char[BUFFSIZE]; was used in previous code
+    // Write and read data over network
+
+
+    //	Send a message to the server letting it know the number of iterations of the test it will perform
+    for (int i = 0; i < BUFFSIZE; i++)
+    {
+        databuf[i] = 'z';
+    }
+
+    //Perform the appropriate number of tests with the server (measure the time this takes;
+    // I would recommend the  chrono time library)
+    int bytesWritten = write(clientSD, databuf, BUFFSIZE);
+    cout << "Bytes Written: " << bytesWritten << endl;
+
+    //Receive from the server a message with the number read() system calls it performed
+    int bytesRead = read(clientSD, databuf, BUFFSIZE);
+    cout << "Bytes Read: " << bytesRead << endl;
+    cout << databuf[13] << endl;
+
+
+    //Print information about the test (use chrono library to time the
+    //Test (1,2, or 3): time = xx usec, #reads = yy, throughput zz Gbps
+    close(clientSD);
+    return 0;
+}
+
+
+
--- Server.cpp
+++ Server.cpp
@@ -18,15 +18,67 @@
 * (3) sending the number of reads which the server performed.
 */
 
-// *
-// validate the arguments
-//	if ( argc != 2 )
-//	{
-//		printf( "usage: server #port\n" );
-//		return -1;
-//	}
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+#include <netdb.h>
+#include <netinet/tcp.h>
+#include <sys/uio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <pthread.h>
+using namespace std;
 
-/*
+const int BUFFSIZE = 1500;
+const int NUM_CONNECTIONS = 5;
+
+struct clientDataArgs{
+    int clientSocket;
+    char* databuf;
+    //int iterations;
+};
+
+void *thread_runner (void *args)
+{
+    int number_of_reads = 0;
+    int total_bytes_read = 0;
+    //Receive a message by the client with the number of iterations to perform
+    clientDataArgs clientData = *(clientDataArgs*) args;
+    int bytesRead = read(clientData.clientSocket, clientData.databuf, BUFFSIZE);
+    //Receive a message by the client with the number of iterations to perform
+    int iteration = clientData.databuf[0];
+    cout << "iteration: " << iteration << endl;
+    bzero(clientData.databuf, BUFFSIZE); //zeroed out buffer
+
+    //Read from the client the appropriate number of iterations of BUFSIZE amounts of data
+    //Note: the read system call may return without reading the entire data buffer.
+    //You must repeat calling read until you have read a BUFSIZE amount of data.
+    //This should be done for each repetition on the server.
+
+    while(total_bytes_read != BUFFSIZE){
+        bytesRead = read(clientData.clientSocket, clientData.databuf, BUFFSIZE);
+        total_bytes_read += bytesRead;
+        if(bytesRead > 0){
+            number_of_reads++ ;
+        }
+    }
+
+    //	Send the number of read() calls made as an acknowledgment to the client
+    clientData.databuf[0] = number_of_reads;
+    int bytesWritten = write(clientData.clientSocket, clientData.databuf, BUFFSIZE);
+  //  cout << "Bytes Written: " << bytesWritten << endl;
+
+    close(clientData.clientSocket);
+
+}
+
+int main(int argc, char *argv[])
+{
+    /*
  * The servicing thread will do the following:
 1.	Allocate dataBuf[BUFSIZE], where BUFSIZE = 1500 to read in data being sent by client
 2.	Receive a message by the client with the number of iterations to perform
@@ -38,3 +90,76 @@ Note: the read system call may return without reading the entire data buffer.
 6.	Terminate the thread
 
  */
+
+    //validate the arguments
+    if ( argc != 2 )
+    {
+        printf( "usage: server #port\n" );
+        return -1;
+    }
+
+    int serverPort =  atoi(argv[1]);
+    //char *serverName;
+
+    pthread_t thread[NUM_CONNECTIONS];
+    int thread_num = 0;
+
+    //Build address
+    //int port = 12345;
+    sockaddr_in acceptSocketAddress;
+    bzero((char *)&acceptSocketAddress, sizeof(acceptSocketAddress));
+    acceptSocketAddress.sin_family = AF_INET; //use the internet
+    acceptSocketAddress.sin_addr.s_addr = htonl(INADDR_ANY); //server listen for any ip address thats trying to connect
+    acceptSocketAddress.sin_port = htons(serverPort); //port address
+
+
+    //Create socket
+    int serverSD = socket(AF_INET, SOCK_STREAM, 0); //sock_stream is use tcp_ip
+    const int on = 1;
+    setsockopt(serverSD, SOL_SOCKET, SO_REUSEADDR, (char *)&on, sizeof(int));
+    cout << "Socket #: " << serverSD << endl;
+
+    //Bind
+    int rc = bind(serverSD, (sockaddr *)&acceptSocketAddress, sizeof(acceptSocketAddress));
+    if (rc < 0)
+    {
+        cerr << "Bind Failed" << endl;
+    }
+
+//    The main thread of a server will run in a dispatch loop which:
+//    •	Accepts a new connection.
+//    •
+
+    //listen and accept
+    listen(serverSD, NUM_CONNECTIONS);       //setting number of pending connections
+
+    while(1)
+    {
+        sockaddr_in newSockAddr;
+        socklen_t newSockAddrSize = sizeof(newSockAddr);
+        int newSD = accept(serverSD, (sockaddr *) &newSockAddr, &newSockAddrSize); //socket of client is returned
+        if (newSD == -1) {
+            std::cerr << "Error while Accepting on socket\n";
+            continue;
+        }
+
+       // cout << "Accepted Socket #: " << newSD <<endl;
+
+        //Creates a struct with client information
+        struct clientDataArgs *args = new clientDataArgs;
+        args->clientSocket = newSD;
+        char databuf[BUFFSIZE];
+        bzero(databuf, BUFFSIZE); //zeroed out buffer
+        args->databuf = databuf;
+
+        //Creates a thread to service that request
+        pthread_create(&thread[thread_num], NULL, thread_runner, (void*) args);
+        thread_num++;
+        //terminate thread
+        pthread_join(thread[thread_num], NULL );
+
+    }
+
+    return 0;
+}
+
